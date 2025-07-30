f1f8ebc7f3fc8d131b355459d5348e42e67fe401
JeffShu <674064220@qq.com>
2025-07-30 10:13:16 +0800

    slice

--- chaapter01/07-slice.go
+++ chaapter01/07-slice.go
@@ -40,5 +40,25 @@ func main() {
 	fmt.Println("s3", s3)
 	s4 := s1[:copy(s1, s1[3:])] // 删除开头前三个元素
 	fmt.Println("s4", s4)
+	fmt.Println("------------------")
 	// 数据共享
+	// 它们的数组指针指向了同一个数组,所以会同步修改
+	// s5: [1 2 6 4 5]
+	// s6: [2 6]
+	s5 := []int{1, 2, 3, 4, 5}
+	s6 := s5[1:3]
+	s6[1] = 6
+	fmt.Println("s5:", s5)
+	fmt.Println("s6:", s6)
+	fmt.Println("------------------")
+	// 解决数据共享同步修改问题
+	// 通过 append 创建一个新的切片,从而避免数据共享
+	// 没有进行扩容是不会重新分配内存的也就还有数据共享问题
+	s7 := s5[1:]
+	s7 = append(s7, 0)
+	fmt.Println("s5:", s5)
+	fmt.Println("s7:", s7)
+	s7[2] = 100
+	fmt.Println("s5:", s5)
+	fmt.Println("s7:", s7)
 }
